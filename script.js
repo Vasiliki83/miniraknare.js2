703b4fae9b65018679844f707965c66ff2bd8a01
Vasiliki83 <vasiliki.panagiotidis@student.kyh.se>
2022-01-31 23:40:31 +0100

    onclick funktion som hämtar värdet från input till paragraf

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-function result() {
-    var points = document.getElementById("ruta").textContent;
-}
+// $(document).ready(function () { // initierar jquery när dokument är redo
+//     // let value = $(#)
+
+// jquert test för input värde som skall skrivas ut i en paragraf ( Calc)
+$(document).on("click", "#add", function () { //onclick function för knapp med id Add
+    let num1 = $("#txtNum").val() // en variabel med innehållet från inputfältet (txtNum)
+    let numAdd = num1 + "+"; // variabel med innehåll av Num1 och ett + tecken
+    $("#calc").text(numAdd); // skriver ut värdet av numAdd i paragrafen med id calc
+
+
+
+
+});
